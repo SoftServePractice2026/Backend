0d04daf91f84eee670e735c950d5903b3f9880be
QusDev <qusdeveloper@gmail.com>
2026-01-13 02:49:46 +0200

    Init commit

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var currentAssembly = typeof(DependencyInjection).Assembly;
+
+        services.AddValidatorsFromAssembly(currentAssembly);
+
+        return services;
+    }
+}
