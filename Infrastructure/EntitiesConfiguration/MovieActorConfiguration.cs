578b99b846f86bd486c5ed96dbc7d690101353de
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2026-01-16 00:44:36 +0200

    created movie actor configuration and defined many-to-one relations to movie and actor

---
+++ Infrastructure/EntitiesConfiguration/MovieActorConfiguration.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.Entities;
+
+
+namespace Infrastructure.EntitiesConfiguration
+{
+    public class MovieActorConfiguration : IEntityTypeConfiguration<MovieActorEntity>
+    {
+        public void Configure(EntityTypeBuilder<MovieActorEntity> builder)
+        {
+            builder.ToTable("movie_actors");
+            
+            builder.HasKey(h => h.Id);
+            builder.Property(h => h.Id)
+                .HasColumnName("movie_actors_id");
+            
+            
+            builder.Property(ma => ma.ActorId)
+                .HasColumnName("actor_id")
+                .IsRequired();
+
+            
+            builder.HasIndex(ma => new { ma.MovieId, ma.ActorId })
+                .IsUnique();
+
+
+            builder.HasOne(ma => ma.Movie)
+                .WithMany(m => m.ActorsInMovies)
+                .HasForeignKey(ma => ma.MovieId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            builder.HasOne(ma => ma.Actor)
+                .WithMany(a => a.ActorsInMovies)
+                .HasForeignKey(ma => ma.ActorId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
