a29aa56fd00d9918d2247012b0e40b034ca11d03
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2026-01-16 00:43:08 +0200

    made hall name required, initialized navigation collections, created HallConfiguration

--- Domain/Entities/HallEntity.cs
+++ Domain/Entities/HallEntity.cs
@@ -7,16 +7,17 @@ namespace Domain.Entities
 {
     public class HallEntity:Entity
     {
-        public string? Name { get; set; }
+        public string Name { get; set; } = null!;
 
         public int Raws { get; set; }
 
         public int SeatsPerRaw { get; set; }
 
         public bool IsActive { get; set; }
+        
+        public ICollection<SessionEntity> Sessions { get; set; } = new List<SessionEntity>();
+        
+        public ICollection<SeatEntity> Seats { get; set; } = new List<SeatEntity>();
 
-        public ICollection<SessionEntity> Sessions { get; set; }
-
-        public ICollection<SeatEntity> Seats { get; set; }
     }
 }
