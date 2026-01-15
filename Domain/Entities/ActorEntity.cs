59e54869b40d5b31fbc0c63ae04f008d6b38dfcd
Anna-Yevtushenko <ayevtushenko@kse.org.ua>
2026-01-16 00:24:58 +0200

    added ActorConfiguration and made first and last name required

--- Domain/Entities/ActorEntity.cs
+++ Domain/Entities/ActorEntity.cs
@@ -7,10 +7,8 @@ namespace Domain.Entities
 {
     public class ActorEntity: Entity
     {
-        public string? FirstName { get; set; }
-
-        public string? LastName { get; set; }
-
-        public ICollection<MovieActorEntity> ActorsInMovies { get; set; }    
+        public string FirstName { get; set; } = null!;
+        public string LastName  { get; set; } = null!;
+        public ICollection<MovieActorEntity> ActorsInMovies { get; set; } = new List<MovieActorEntity>();
     }
 }
