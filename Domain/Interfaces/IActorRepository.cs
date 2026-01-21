002f57934b3ea7d9e232e978b6b4802e78cc39ec
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-01-21 16:59:02 +0100

    add iactorrepository interface

---
+++ Domain/Interfaces/IActorRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface IActorRepository
+    {
+        Task CreateActorAsync(ActorEntity actorEntity);
+        Task DeleteActorAsync(ActorEntity actorEntity);
+        Task UpdateActorAsync(ActorEntity actorEntity);
+        Task<ActorEntity?> GetActorByIdAsync(Guid actorId);
+        Task<IReadOnlyList<ActorEntity>> FindActorsByNameAsync(string actorName);
+        Task<IReadOnlyList<ActorEntity>> FindActorsBySurnameAsync(string actorSurname);
+        Task<IReadOnlyList<ActorEntity>> FindActorsByFullNameAsync(string actorName, string actorSurname);
+        Task<IReadOnlyList<ActorEntity>> GetActorEntitiesAsync();
+        Task SaveChangesAsync();
+
+    }
+}
