bf38e05df9673557d421c861bebbeb3099f3fadb
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-01-22 16:34:00 +0100

    add validators for dtos

---
+++ Application/Validators/Genres/GenreCreateDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Validators.Genres
+{
+    public class GenreCreateDtoValidator : AbstractValidator<GenreCreateDto>
+    {
+        public GenreCreateDtoValidator()
+        {
+            RuleFor(h => h.Name)
+                .NotEmpty().WithMessage("Genre name is required").WithErrorCode("name.empty")
+                .MinimumLength(1)
+                .MaximumLength(30);
+
+        }
+    }
+}
