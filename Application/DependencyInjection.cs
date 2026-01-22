43bdab2ecd14becc245c3d967e5a8beda94a7d61
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-01-22 11:21:39 +0100

    add dependency 1 injection for service

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Services;
+using Application.Validators.Actors;
 using Application.Validators.Halls;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,12 +14,15 @@ public static class DependencyInjection
 
         //Dependency all validators in current assembly
         services.AddValidatorsFromAssemblyContaining<HallCreateDtoValidator>();
+        services.AddValidatorsFromAssemblyContaining<ActorCreateDtoValidator>();
 
         //Dependency all mapping profiles in current assembly
         services.AddAutoMapper(cfg => { }, currentAssembly);
+        services.AddAutoMapper(acg => { }, currentAssembly);
 
         //Dependency services
         services.AddScoped<IHallService, HallService>();
+        services.AddScoped<IActorService, ActorService>();
 
         return services;
     }
