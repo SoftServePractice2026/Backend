ebad233e513b65809b8d21e83d7de50e087410fa
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-01-22 17:13:40 +0100

    add controller for genre

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Services;
 using Application.Validators.Actors;
+using Application.Validators.Genres;
 using Application.Validators.Halls;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,15 +16,15 @@ public static class DependencyInjection
         //Dependency all validators in current assembly
         services.AddValidatorsFromAssemblyContaining<HallCreateDtoValidator>();
         services.AddValidatorsFromAssemblyContaining<ActorCreateDtoValidator>();
-
+        services.AddValidatorsFromAssemblyContaining<GenreCreateDtoValidator>();
         //Dependency all mapping profiles in current assembly
         services.AddAutoMapper(cfg => { }, currentAssembly);
         services.AddAutoMapper(acg => { }, currentAssembly);
-
+        services.AddAutoMapper(acg => { }, currentAssembly);
         //Dependency services
         services.AddScoped<IHallService, HallService>();
         services.AddScoped<IActorService, ActorService>();
-
+        services.AddScoped<IGenreService, GenreService>();
         return services;
     }
 }
---
+++ WebAPI/Controllers/GenreController.cs
@@ -0,0 +1,70 @@
+﻿using Application.DTOs;
+using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Mappers;
+
+namespace WebAPI.Controllers
+{
+    public class GenreController : BaseController
+    {
+        private readonly IGenreService _genreService;
+
+        public GenreController(IGenreService genreService)
+        {
+            _genreService = genreService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] GenreCreateDto dto)
+        {
+            var result = await _genreService.CreateGenreAsync(dto);
+
+            return result.IsFailure
+                ? FailureMapper.ToHttp(result.Failure!)
+                : Ok(result.Value);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(Guid id, GenreUpdateDto dto)
+        {
+            var result = await _genreService.UpdateGenreAsync(id, dto);
+            return result.IsFailure
+                ? FailureMapper.ToHttp(result.Failure!)
+                : Ok(result.Value);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await _genreService.DeleteGenreAsync(id);
+            return result.IsFailure
+                ? FailureMapper.ToHttp(result.Failure!)
+                : NoContent();
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _genreService.GetGenreByIdAsync(id);
+            return result.IsFailure
+                ? FailureMapper.ToHttp(result.Failure!)
+                : Ok(result.Value);
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            var result = await _genreService.GetGenreByNameAsync(name);
+            return result.IsFailure
+                ? FailureMapper.ToHttp(result.Failure!)
+                : Ok(result.Value);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _genreService.GetGenreAllAsync();
+            return Ok(result.Value);
+        }
+    }
+}
