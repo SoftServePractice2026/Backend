5a5bd4aee1ccd29a27a773e89439fe35ce33c25d
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-01-26 11:55:03 +0100

    add actor filter and interface iactorrepository

---
+++ Domain/Filters/ActorFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Filters
+{
+    public class ActorFilter
+    {
+        public string? SearchTerm { get; init; }
+
+        public Guid? MovieId { get; init; }
+
+        public int Page { get; init; } = 1;
+        public int PageSize { get; init; } = 10;
+    }
+}
