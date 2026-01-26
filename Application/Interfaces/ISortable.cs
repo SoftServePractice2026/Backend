05ae2f8ed3761a6d2d3b23446eba715577a6b9f4
QusDev <qusdeveloper@gmail.com>
2026-01-26 23:54:10 +0200

    realize sort by to hall, refactoring pagination

---
+++ Application/Interfaces/ISortable.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Common;
+
+namespace Application.Interfaces
+{
+    public interface ISortable
+    {
+        string? OrderBy { get; }
+        SortDirection SortDirection { get; }
+    }
+}
---
+++ Application/Validators/Common/PaginationBaseDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Common;
+using FluentValidation;
+
+namespace Application.Validators.Common
+{
+    public abstract class PaginationBaseDtoValidator<T> : AbstractValidator<T> where T : PaginationBaseDto
+    {
+        public PaginationBaseDtoValidator()
+        {
+            RuleFor(p => p.PageNumber)
+                .GreaterThan(0);
+
+            RuleFor(p => p.PageSize)
+                .GreaterThan(0)
+                .LessThan(30);
+        }
+    }
+}
