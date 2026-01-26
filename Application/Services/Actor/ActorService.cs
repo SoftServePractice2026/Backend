6e1b812ac866eba3d151a0b286da16a4ff83b49b
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-01-26 13:50:30 +0100

    refactor actor service

--- Application/Services/Actor/ActorService.cs
+++ Application/Services/Actor/ActorService.cs
@@ -1,6 +1,8 @@
-﻿using Application.DTOs.Actor;
+﻿using Application.DTOs;
+using Application.DTOs.Actor;
 using AutoMapper;
 using Domain.Entities;
+using Domain.Filters;
 using Domain.Interfaces;
 using Microsoft.Extensions.Logging;
 using Shared;
@@ -26,29 +28,93 @@ namespace Application.Services.Actor
             _logger = logger;
         }
 
-        public Task<Result<ActorDetailsDto>> CreateActorAsync(ActorCreateDto dto, CancellationToken cancellationToken)
+        public async Task<Result<ActorDetailsDto>> CreateActorAsync(ActorCreateDto dto, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var actor = _mapper.Map<ActorEntity>(dto);
+
+            _repository.CreateActor(actor);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            var resultDto = _mapper.Map<ActorDetailsDto>(actor);
+
+            return Result<ActorDetailsDto>.Success(resultDto);
         }
 
-        public Task<Result<bool>> DeleteActorAsync(Guid id, CancellationToken cancellationToken)
+        public async Task<Result<bool>> DeleteActorAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var actor = await _repository.GetActorByIdAsync(id, cancellationToken);
+
+            if (actor is null)
+            {
+                var actorErorr = Error.NotFound("actor.not.found", $"Actor with id: {id} not found");
+                _logger.LogWarning("Delete actor not found. ActorId={ActorId}, Code = {Code}", id, actorErorr.Code);
+                return Result<bool>.Fail(actorErorr);
+            }
+
+            _repository.DeleteActor(actor);
+            await _unitOfWork.SaveChangesAsync();
+
+            return Result<bool>.Success(true);
         }
 
-        public Task<Result<ActorDetailsDto>> GetActorByIdAsync(Guid id, CancellationToken cancellationToken)
+        public async Task<Result<ActorDetailsDto>> GetActorByIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var actor = await _repository.GetActorByIdAsync(id, cancellationToken);
+
+            if (actor is null)
+            {
+                var actorErorr = Error.NotFound("actor.not.found", $"Actor with id: {id} not found");
+                _logger.LogWarning("Get actor by id not found. ActorId={ActorId}, Code = {Code}", id, actorErorr.Code);
+                return Result<ActorDetailsDto>.Fail(actorErorr);
+            }
+
+            var actorDto = _mapper.Map<ActorDetailsDto>(actor);
+
+            return Result<ActorDetailsDto>.Success(actorDto);
         }
 
-        public Task<Result<(List<ActorListItemDto> Actors, int TotalCount)>> GetFilteredHallsAsync(ActorFilterDto actorFilterDto, CancellationToken cancellationToken)
+        public async Task<Result<(List<ActorListItemDto> Actors, int TotalCount)>> GetFilteredHallsAsync(ActorFilterDto actorFilterDto, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var actorFilter = _mapper.Map<ActorFilter>(actorFilterDto);
+
+            var actors = await _repository.GetFilteredActorsAsync(actorFilter, cancellationToken);
+
+            if (!actors.Any())
+            {
+                var actorsErorr = Error.NotFound("actors.not.found", $"Actors with filter no found");
+                _logger.LogWarning("Get filtered actors not found. SearchTerm={SearcTerm}, MovieId={MovieId}, Code={Code}",
+                    actorFilter.SearchTerm, actorFilter.MovieId, actorsErorr.Code);
+                return Result<(List<ActorListItemDto> Actors, int TotalCount)>.Fail(actorsErorr);
+            }
+
+            var totalCount = await _repository.CountFilteredAsync(actorFilter, cancellationToken);
+
+
+            var actorsDto = _mapper.Map<List<ActorListItemDto>>(actors);
+
+            return Result<(List<ActorListItemDto> Actors, int TotalCount)>.Success((actorsDto, totalCount));
         }
 
-        public Task<Result<ActorDetailsDto>> UpdateActorAsync(Guid targetId, ActorUpdateDto dto, CancellationToken cancellationToken)
+        public async Task<Result<ActorDetailsDto>> UpdateActorAsync(Guid targetId, ActorUpdateDto dto, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var actor = await _repository.GetActorByIdAsync(targetId, cancellationToken);
+
+            if (actor is null)
+            {
+                var actorErorr = Error.NotFound("actor.not.found", $"Actor with id: {targetId} not found");
+                _logger.LogWarning("Update actor not found. ActorId={ActorId}, Code = {Code}", targetId, actorErorr.Code);
+                return Result<ActorDetailsDto>.Fail(actorErorr);
+            }
+
+
+            _mapper.Map(dto, actor);
+
+            _repository.UpdateActor(actor);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            var updatedActorDto = _mapper.Map<ActorDetailsDto>(actor);
+
+            return Result<ActorDetailsDto>.Success(updatedActorDto);
         }
     }
 }
