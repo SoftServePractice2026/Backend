a640a36ea5f12851f535939b0302c54835f721a9
Daria-Korotych <dkorotych@kse.org.ua>
2026-01-28 18:42:29 +0100

    feat: add Seat DTOs

---
+++ Application/DTOs/Seat/SeatListDto.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.Enums;
+
+namespace Application.DTOs.Seat;
+
+public record SeatListDto(
+    Guid Id,
+    int RowNumber,
+    int SeatNumber,
+    SeatTypeEnum SeatType,
+    SeatStatusEnum SeatStatus
+);
