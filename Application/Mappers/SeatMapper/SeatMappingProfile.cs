4c1397eacbd85f3638d05f4e12a8ca934e0e29c3
Daria-Korotych <dkorotych@kse.org.ua>
2026-01-28 18:43:34 +0100

    feat: add Seat validators and mapper; add to SeatStatusEnum new status -> Normal

---
+++ Application/Mappers/SeatMapper/SeatMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using Application.DTOs.Seat;
+using Application.DTOs.Seat.SeatCRUD;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Mappers.SeatMapper;
+
+public class SeatMappingProfile : Profile
+{
+    public SeatMappingProfile()
+    {
+        // Entity -> DTO
+        CreateMap<SeatEntity, SeatDto>();
+        
+        CreateMap<SeatEntity, SeatListDto>();
+
+        // DTO -> Entity
+        CreateMap<CreateSeatDto, SeatEntity>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Hall, opt => opt.Ignore())
+            .ForMember(dest => dest.Tickets, opt => opt.Ignore());
+
+        CreateMap<UpdateSeatDto, SeatEntity>()
+            .ForMember(dest => dest.HallId, opt => opt.Ignore())
+            .ForMember(dest => dest.Hall, opt => opt.Ignore())
+            .ForMember(dest => dest.Tickets, opt => opt.Ignore());
+    }
+}
