3b5fb3557b842e8d83f3c018424d4d01547a295b
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-02-02 08:59:33 +0100

    add dto for external movie

---
+++ Application/DTOs/ExternalMovieDto/ExternalMovie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs.ExternalMovieDto
+{
+    public record ExternalMovieDto(
+    int TmdbId,
+    string Title,
+    string Description,
+    string Poster,
+    int Duration,
+    string Language,
+    decimal Rating,
+    string AgeRating,
+    List<string> Genres,
+    List<ExternalActorDto> Cast
+    );
+
+    public record ExternalActorDto(
+        int TmdbId,
+        string FullName,
+        string PhotoUrl
+        );
+}
