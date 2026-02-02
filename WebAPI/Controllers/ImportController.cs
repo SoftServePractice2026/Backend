63028b9376e0ad41ed31cbaef586899d3b525cba
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-02-02 11:56:10 +0100

    add controller

---
+++ WebAPI/Controllers/ImportController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services.ExternalMovie;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImportController : ControllerBase
+    {
+        private readonly IMovieImportService _importService;
+
+        public ImportController(IMovieImportService importService)
+        {
+            _importService = importService;
+        }
+
+        [HttpPost("populate")]
+        public async Task<IActionResult> PopulateMovies([FromQuery] int pages = 1)
+        {
+            await _importService.ImportFromTmdbAsync(pages);
+            return Ok($"Import initiated for {pages} pages.");
+        }
+    }
+}
