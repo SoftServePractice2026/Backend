746085e0762088045de9f157300a017a28373e65
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-02-05 17:29:42 +0100

    add interface for email service

---
+++ Application/Services/Email/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs.ContactMessage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services.Email
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendEmailAsync(ContactMessageDto contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Services/Email/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs.ContactMessage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Services.Email
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(ContactMessageDto contact);
+    }
+}
