420b8660fe281fd3404255b2257fc48b7163f4c3
kosyk.maxym <kosyk.maxym@lll.kpi.ua>
2026-02-06 09:15:51 +0100

    add service for email sender

--- Application/Services/Email/EmailService.cs
+++ Application/Services/Email/EmailService.cs
@@ -1,15 +1,28 @@
 ﻿using Application.DTOs.ContactMessage;
+using MimeKit;
+using MimeKit.Text;
 using System;
 using System.Collections.Generic;
+using System.Net.Mail;
 using System.Text;
 
 namespace Application.Services.Email
 {
     public class EmailService : IEmailService
     {
-        public Task SendEmailAsync(ContactMessageDto contact)
+        public async Task SendEmailAsync(ContactMessageDto contact)
         {
-            throw new NotImplementedException();
+            var message = new MimeMessage();
+            message.From.Add(MailboxAddress.Parse(contact.Email));
+            message.To.Add(MailboxAddress.Parse("cinemadmin@gmail.com"));
+            message.Subject = $"User: {contact.Name}";
+            message.Body = new TextPart(TextFormat.Plain) { Text = $"""{contact.Message}"""};
+
+
+            using var smtp = new MailKit.Net.Smtp.SmtpClient();
+            await smtp.ConnectAsync("localhost", 8025);
+            await smtp.SendAsync(message);
+            await smtp.DisconnectAsync(true);
         }
     }
 }
