4e34bfeab46c803f8d6546e086ef34c8ac3c9ad7
mcaleg2006 <emil720apro@gmail.com>
2026-02-11 13:25:24 +0200

    calendar logic final version change session

--- Domain/Filters/SessionFilter.cs
+++ Domain/Filters/SessionFilter.cs
@@ -8,6 +8,8 @@ public class SessionFilter
     public Guid? MovieId { get; init; }
     public Guid? HallId { get; init; }
     public SessionStatusEnum? Status { get; init; }
+    
+    public DateTime? Date { get; init; }
     public DateTime? From { get; init; }
     public DateTime? To { get; init; }
     public string? MovieTitle { get; init; }
